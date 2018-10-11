65b0cd666911f0eea2ae8a7845e0faf287125f71
unknown <villegasoshin25@gmail.com>
2018-10-10 19:00:21 -0500

    empezando

---
+++ To-Do/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+//firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule} from 'angularfire2/database';
+import {environment } from '../environments/environment';
+//components
+import { ProductsComponent } from './components/products/products.component';
+import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { ProductComponent } from './components/products/product/product.component'
+
+//services
+import { ProductService } from './services/product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
+  ],
+  providers: [
+    ProductService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ To-Do/src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyB1_AjZfDIzzHx6LVZxZwAx2_kTabybOWI",
+    authDomain: "to-do-5549b.firebaseapp.com",
+    databaseURL: "https://to-do-5549b.firebaseio.com",
+    projectId: "to-do-5549b",
+    storageBucket: "to-do-5549b.appspot.com",
+    messagingSenderId: "277838843796"
+   }
+  };
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
